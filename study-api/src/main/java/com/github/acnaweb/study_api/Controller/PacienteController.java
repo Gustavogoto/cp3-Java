0d4071475878d731769c78f21b162d2f81bc0dda
gustavoakio <gustavoakioalves@gmail.com>
2025-05-05 21:33:47 -0300

    fix import

--- study-api/src/main/java/com/github/acnaweb/study_api/Controller/PacienteController.java
+++ study-api/src/main/java/com/github/acnaweb/study_api/Controller/PacienteController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,17 +18,16 @@ import com.github.acnaweb.study_api.Service.PacienteService;
 import com.github.acnaweb.study_api.dto.PacienteCreate;
 import com.github.acnaweb.study_api.dto.PacienteResponse;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
-
 @RestController
-@RequestMapping("paciente")
+@RequestMapping("pacientes")
 public class PacienteController {
 
      @Autowired
      PacienteService ps;
-
+     
     @PostMapping
     public  ResponseEntity<PacienteResponse> create(@RequestBody PacienteCreate pc){
+        System.out.println(pc);
         PacienteResponse pr = new PacienteResponse(ps.create(pc));
         return ResponseEntity.status(201).body(pr);
     }
